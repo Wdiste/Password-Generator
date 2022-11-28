972c33ff6672a475b1b86a92a3e355830a8ca9d1
wdiste <Wdiste49@gmail.com>
2022-11-28 15:54:35 -0500

    Reworked checks for NaN.  Due to variable element being named 'length' it was throwing errors.  Attribute renamed 'len' in passParams object.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,7 @@ var upperCaseLib = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 var numLib = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specCharLib = [' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',','-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '\`', '{', '|', '}', '~'];
 var passParams = [
-  length = 8,
+  len = 8,
   lowCase = false,
   upCase = false,
   num = false,
@@ -29,25 +29,31 @@ function generatePassword() {
   passParams.num = confirm("Would you like to include numbers in your password?");
   passParams.spec = confirm("Would you like to include special characters in your password?");
 
+  // check to see at least one option is selected
   if(passParams.lowCase == false && passParams.upCase == false && 
      passParams.num == false && passParams.spec == false) {
       alert("Error: please select at least one character type")
       return "Please select at least one character type";
-     }
+     };
 
-  passParams.length = parseInt(prompt("Choose a length for your password between 8 and 128 characters."))
+  // ask user to input number of chars
+  passParams.len = parseInt(prompt("Choose a length for your password between 8 and 128 characters."));
 
-  if (passParams.length.isNaN || (passParams.length < 8 || passParams.length > 128)) {
+  // make sure prompt is a number and between 8 and 128
+  if (isNaN(passParams.len) || (passParams.len < 8 || passParams.len > 128)) {
     alert("Please enter a number between 8 and 128");
     return "Please enter a number between 8 and 128";
   };
+
+  // if all checks are cleared
+
 
   // log all inputs for dev use
   console.log(passParams.lowCase);
   console.log(passParams.upCase);
   console.log(passParams.num);
   console.log(passParams.spec);
-  console.log(passParams.length);
+  console.log(passParams.len);
 };
 
 
