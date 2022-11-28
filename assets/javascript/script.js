27a383ddad3d8cc2d1f078d2402c26b605df9f0f
wdiste <Wdiste49@gmail.com>
2022-11-28 14:32:50 -0500

    Added functionality to generatePassword().  Will prompt for input and throw error if outside parameters.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passParams = [
+  length = 8,
+  lowCase = false,
+  upCase = false,
+  num = false,
+  spec = false,
+];
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +15,18 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
+
+function generatePassword() {
+  passParams.length = parseInt(prompt("Choose a length for your password between 8 and 128 characters."))
+  console.log(passParams.length);
+  if (passParams.length.isNaN || (passParams.length < 8 || passParams.length > 128)) {
+    return "Please enter a number between 8 and 128";
+  };
+};
+
+
+// -----------------------  MAIN  -----------------------
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
